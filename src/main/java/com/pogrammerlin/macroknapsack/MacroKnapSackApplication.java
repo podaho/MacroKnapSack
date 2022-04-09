6e530cdcd3aba05220b0682e2eac8ffb87bacc63
Po Lin <po.grammer.lin@gmail.com>
2022-04-08 22:28:13 -0500

    Boilerplate commit

---
+++ src/main/java/com/pogrammerlin/macroknapsack/MacroKnapSackApplication.java
@@ -0,0 +1,13 @@
+package com.pogrammerlin.macroknapsack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroKnapSackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacroKnapSackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pogrammerlin/macroknapsack/MacroKnapSackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pogrammerlin.macroknapsack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroKnapSackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
