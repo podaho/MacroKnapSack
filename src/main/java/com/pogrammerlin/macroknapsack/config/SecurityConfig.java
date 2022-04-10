5d7232f2db8522b4cd0ba7fa126699702855c993
Po Lin <po.grammer.lin@gmail.com>
2022-04-09 22:49:26 -0500

    Adding basic auth and fixing tests

--- src/main/java/com/pogrammerlin/macroknapsack/config/SecurityConfig.java
+++ src/main/java/com/pogrammerlin/macroknapsack/config/SecurityConfig.java
@@ -1,15 +1,43 @@
 package com.pogrammerlin.macroknapsack.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${basicAuth.userId}")
+    private String userId;
+
+    @Value("${basicAuth.password}")
+    private String password;
+
     @Override
-    protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable();
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(userId).password("{noop}"+password)
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable()
+                .httpBasic();
+
+        http.addFilterAfter(new CustomFilter(),
+                BasicAuthenticationFilter.class);
     }
 }
--- src/test/java/com/pogrammerlin/macroknapsack/HttpResponseCheck.java
+++ src/test/java/com/pogrammerlin/macroknapsack/HttpResponseCheck.java
@@ -1,16 +1,16 @@
 package com.pogrammerlin.macroknapsack;
 
-import com.pogrammerlin.macroknapsack.client.FatSecret.dto.FatSecretSearchResponse;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +25,16 @@ public class HttpResponseCheck {
 
     @Test
     public void checkDefaultHelloWorldEndpoint() {
-        ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:"+port+"/default/helloWorld?say=testString", String.class);
+        HttpEntity<String> request = new HttpEntity<>(getBasicAuthHeaders());
+        ResponseEntity<String> response = testRestTemplate.exchange("http://localhost:"+port+"/default/helloWorld?say=testString", HttpMethod.GET, request, String.class);
+//                testRestTemplate.getForEntity("http://localhost:"+port+"/default/helloWorld?say=testString", String.class);
         assertEquals("Hello Word! My name is Test Name, and to you I say testString", response.getBody());
+    }
+
+    private HttpHeaders getBasicAuthHeaders() {
+        String basicCreds = "testAdmin:testAdmin";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic "+new String(Base64.encodeBase64(basicCreds.getBytes())));
+        return headers;
     }
 }
